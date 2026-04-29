e1b9533f1063a0d2c3fed0e1c271a38e5ced5638
sonamanayakkar <esonaman2@gmail.com>
2026-04-30 01:09:25 +0530

    root path changed2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Smart-Farming-and-Market-Management-System">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
 
